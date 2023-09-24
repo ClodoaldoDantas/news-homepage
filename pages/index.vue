d39f2952d4d8e67a361c6006c907b2298b3ad6f7
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-09-23 22:52:27 -0300

    feat: global styles

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Head>
+    <Title>News | Homepage</Title>
+    <Meta name="description" content="Frontend Mentor Challenge" />
+  </Head>
+
+  <h1>The Bright Future of Web 3.0?</h1>
+
+  <p>
+    We dive into the next evolution of the web that claims to put the power of
+    the platforms back into the hands of the people. But is it really fulfilling
+    its promise?
+  </p>
+</template>
