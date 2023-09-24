7631aa62a55f46b061e9c1456d93c2fc0e6d3cfa
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-09-24 09:23:21 -0300

    feat: header componente (mobile)

---
+++ components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <img class="header__logo" src="/images/logo.svg" alt="News" />
+
+      <button class="header__trigger" type="button" aria-label="Open menu">
+        <img src="~/assets/icons/icon-menu.svg" alt="" />
+      </button>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 1.75rem 0 2rem;
+
+  &__logo {
+    width: 44px;
+    height: 28px;
+  }
+
+  & > .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__trigger {
+    appearance: none;
+    border: 0;
+  }
+}
+</style>
